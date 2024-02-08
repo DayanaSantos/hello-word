281f1e9324af8e057ebfa2d115b5bfdaeee6a81e
Dayana Santos <dayanasantosdasilva@gmail.com>
2024-02-07 21:24:33 -0300

    criacao de css e ajuste das classes no jsx

--- src/pages /SobrePage/SobreMimPage/index.jsx
+++ src/pages /SobrePage/SobreMimPage/index.jsx
@@ -3,23 +3,24 @@ import "./styles.css"
 const SobreMimPage = () => {
   return (
     <>
-      
-      <div className="card"> 
-      <img src="../../../public/sobremim.png" className="cardAvatar"alt="..."/>
-       <div className="cardBody">
-        <h2 className="cardTittle">Dayana Santos</h2>
-        <p className="cardTex">Me chamo Dayana Santos, tenho 31 anos, sou natural de Recife, PE e há um ano venho estudando programação com foco em Front-End. Sou formada em técnico em enfermagem e especialista em enfermagem do trabalho onde aprendi a trabalhar em equipe. Integrei um projeto de inclusão digital onde atuei como professora e coordenadora onde adquiri vearias habilidades, entre elas: gestão de projeto, liderança (...)</p>
-        
-
-     </div>
+      <div className="card">
+        <div className="cardAvatar">
+          <img src="../../../public/sobremim.png" alt="..." />
+        </div>
+        <div className="cardBody">
+          <h2 className="cardTittle">Dayana Santos</h2>
+          <p className="cardTex">
+            Tenho 31 anos, sou natural de Recife, e
+            há um ano venho estudando programação com foco em Front-End. Sou
+            formada em técnico em enfermagem e especialista em enfermagem do
+            trabalho onde aprendi a trabalhar em equipe. Integrei um projeto de
+            inclusão digital onde atuei como professora e coordenadora onde
+            adquiri vearias habilidades, entre elas: gestão de projeto e 
+            liderança
+          </p>
+        </div>
       </div>
 
-      <p className="mt-5 text-center">Me acompanhe: <a href="https://www.linkedin.com/in/dayana-santos-6330b2bb/" target="_blank"> Linkedin</a></p>
-
-      
-
-    
-    
     </>
   );
     
