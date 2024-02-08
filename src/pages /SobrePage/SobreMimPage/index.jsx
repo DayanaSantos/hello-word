60167c3b7be25bf1d9009354253441d83432d0ba
Dayana Santos <dayanasantosdasilva@gmail.com>
2024-02-07 21:51:23 -0300

    inseri uma imagem, modifiquei o jsx do sobrepretalab e criei o css

--- src/pages /SobrePage/SobreMimPage/index.jsx
+++ src/pages /SobrePage/SobreMimPage/index.jsx
@@ -5,18 +5,18 @@ const SobreMimPage = () => {
     <>
       <div className="card">
         <div className="cardAvatar">
-          <img src="../../../public/sobremim.png" alt="..." />
+          <img src="../../../public/sobremim.png" alt="foto de uma mulher com cabelo trançado preto" />
         </div>
         <div className="cardBody">
           <h2 className="cardTittle">Dayana Santos</h2>
           <p className="cardTex">
-            Tenho 31 anos, sou natural de Recife, e
+            Sou natural de Recife, e
             há um ano venho estudando programação com foco em Front-End. Sou
             formada em técnico em enfermagem e especialista em enfermagem do
             trabalho onde aprendi a trabalhar em equipe. Integrei um projeto de
             inclusão digital onde atuei como professora e coordenadora onde
-            adquiri vearias habilidades, entre elas: gestão de projeto e 
-            liderança
+            adquiri várias habilidades, como gestão de projeto e 
+            liderança.
           </p>
         </div>
       </div>
--- src/pages /SobrePage/SobrePretaLabPage/index.jsx
+++ src/pages /SobrePage/SobrePretaLabPage/index.jsx
@@ -4,10 +4,15 @@ const SobrePretaLabPage = () => {
   return (
     <>
       
-      <div className="sobrePreta">
-      <h1>É uma plataforma que conecta mulheres negras que são ou gostariam de ser da tecnologia, por meio de ciclos formativos, rede de profissionais, mercado de trabalho, consultorias e estudos. Ponto de vista como participante: Participar do PtetaLab foi o meu ponta pé inicial para adentrar no mundo da técnologia. Fez toda a diferença fazer parte deste ciclo em especial por ser só mulheres pretas, isso gerou um ambiente seguro e acolhedor no que deixou a jornada muito mais fácil</h1>
+      <div className="card">
+        <div className="cardPreta">
+          <img src="../../../public/pretalab.png" alt="logo da pretalab"></img>
+        </div>
+        <div className="cardBody">
+          <h2 className="cardTittle"SobrePretaLabPage></h2>
+      <p className="cardText"> É uma plataforma que conecta mulheres negras que são ou gostariam de ser da tecnologia, por meio de ciclos formativos, rede de profissionais, mercado de trabalho, consultorias e estudos. Participar do PtetaLab foi o meu ponta pé inicial para adentrar no mundo da técnologia. Fez toda a diferença fazer parte deste ciclo em especial por ser só mulheres pretas, isso gerou um ambiente seguro e acolhedor no que deixou a jornada muito mais fácil</p>
 
-        <img src="" />
+      </div>
       </div>
     </>
   );
